a32e540f2ca7a5b75587d84b15c02f6c1914c14f
Ruma543 <ruma.afrin543@gmail.com>
2023-09-15 03:11:15 +0600

    install reactTostify and set toast alert if the course take another time

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import './App.css';
 import Carts from './Component/Carts';
 import Courses from './Component/Courses';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const [selectCarts, setSelectCarts] = useState([]);
@@ -10,23 +12,28 @@ function App() {
   const [price, setPrice] = useState(0);
   const limit = 20;
   const handleSelect = course => {
-    let price = course.course_price;
+    // let price = course.course_price;
     let hour = course.course_credit;
 
-    const isExist = selectCarts.find(item => item.id == course.id);
+    const isExist = selectCarts.find(item => item.id === course.id);
     if (isExist) {
-      return alert('this course is already exist');
+      toast.error('This course is already in your cart', {
+        position: toast.POSITION.TOP_RIGHT,
+      });
     } else {
       selectCarts.forEach(item => {
         hour = hour + item.hour;
       });
       const remainingHour = limit - hour;
-      selectCarts.forEach(item => {
-        price = price + item.price;
-      });
-      if (hour > limit) {
-        return alert('out of your hour limit');
+      // console.log(hour);
+      if (remainingHour < 0 && hour === limit) {
+        toast.error('You are out of your hour limit', {
+          position: toast.POSITION.TOP_RIGHT,
+        });
       } else {
+        // selectCarts.forEach(item => {
+        //   price = price + item.price;
+        // });
         setRemainingHour(remainingHour);
         setHour(hour);
         const newSelectCarts = [...selectCarts, course];
@@ -39,7 +46,9 @@ function App() {
   };
   return (
     <>
-      <h1 className="text-4xl font-bold text-center">Course Registration</h1>
+      <h1 className="text-4xl font-bold text-center my-6">
+        Course Registration
+      </h1>
       <div className="flex w-11/12 mx-auto">
         <Courses handleSelect={handleSelect}></Courses>
         <Carts
@@ -49,8 +58,10 @@ function App() {
           price={price}
         ></Carts>
       </div>
+      <ToastContainer />;
     </>
   );
 }
 
 export default App;
+
--- src/Component/Carts.jsx
+++ src/Component/Carts.jsx
@@ -5,16 +5,22 @@ const Carts = ({ selectCarts, remainingHour, hour, price }) => {
   // console.log(selectCarts);
 
   return (
-    <div className="w-1/4">
-      <h3>Credit Hour Remaining {remainingHour} hr</h3>
+    <div className="w-1/4 px-3">
+      <h3 className="text-xl font-semibold text-blue-700 border-b-2 py-3 border-gray-300">
+        Credit Hour Remaining {remainingHour} hr
+      </h3>
       <h3 className="text-xl font-semibold">Course Name</h3>
-      <div>
+      <div className="border-b-2 border-gray-300 py-3 ">
         {selectCarts.map((cart, idx) => (
           <Cart key={idx} cart={cart}></Cart>
         ))}
       </div>
-      <h3>Total Credit Hour: {hour}</h3>
-      <h3>Total price: {price} USD</h3>
+      <h3 className="text-xl font-semibold text-gray-600 border-b-2 py-3 border-gray-300">
+        Total Credit Hour: {hour}
+      </h3>
+      <h3 className="text-xl font-semibold text-gray-600  py-3">
+        Total price: {price} USD
+      </h3>
     </div>
   );
 };
